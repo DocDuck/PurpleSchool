ef566f53f3e99d267ff61d38f95c3bb859ce836e
DocDuck <34368922+DocDuck@users.noreply.github.com>
2022-04-08 19:20:32 +0500

    Update group.ts
    реализация с пояснениями

--- ts/group.ts
+++ ts/group.ts
@@ -7,8 +7,8 @@ interface IData {
 const data: IData[] = [
     {group: 1, name: 'sdf'},
     {group: 2, name: 'kuymt'},
-    {group: 1, name: 'rtuyyu'},
-    {group: 3, name: 'hnnhnh'},
+    {group: 1, name: 'rtuyyu', options: 666},
+    {group: 3, name: 'hnnhnh', options: 'hello'},
     {group: 1, name: 'fdfdfd'},
 ]
 
@@ -19,7 +19,16 @@ interface IGroup<T> {
 }
 
 const group = <T extends Record<TKey, any>>(arr: T[], key: keyof T): IGroup<T> => {
-    return arr.reduce((map, item) => {
+    return arr.reduce<IGroup<T>>((map, item) => {
+        const itemGroupKey = item[key] // Значение у итерируемого объекта, который станет ключем результирующей группы
+        // Если уже создан ключ группы - значением будет некоторый массив
+        if(Array.isArray(map[itemGroupKey])) {
+            // Добавляем объект в группу
+            map[itemGroupKey].push(item)
+        } else {
+            // Иначе создаем новый массив с текущим значением
+            map[itemGroupKey] = [item]
+        }
         return map
     }, {})
 }
