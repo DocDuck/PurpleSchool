f3e12c032bdd7c24aef9c0d6068511f9db7878d5
DocDuck <34368922+DocDuck@users.noreply.github.com>
2022-04-17 19:36:19 +0500

    upd
    Добавил дескриптор метода

---
+++ ts/decorator.ts
@@ -0,0 +1,24 @@
+@CreatedAt
+class ExampleClass {
+    constructor(private foo: string = '47', public createdAt: number = 0) {
+        this.foo = foo
+    };
+
+    @Log
+    bar() {
+        console.log('Вызов метода бар', this.foo, this.createdAt)
+    }
+}
+
+function CreatedAt<T extends { new(...args: any[]): {}}>(constructor: T) {
+    return class extends constructor {
+        createdAt = new Date().toString()
+    }
+}
+
+function Log(t: Object, pk: string, d: TypedPropertyDescriptor<(...args: any[]) => any>) {
+    console.log(`Метод ${pk} вызван ${new Date().toString()}`)
+    console.log('Дескриптор метода:', JSON.stringify(d))
+}
+
+new ExampleClass().bar()
