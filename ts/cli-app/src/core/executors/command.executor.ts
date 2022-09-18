cfb12768d04024a792f2b86800e87c80ec2b7a2a
DocDuck <alex.mitin.psmu@gmail.com>
2022-09-18 20:38:53 +0500

    command abstract class
    Общий класс для исполнителя консольных команд

---
+++ ts/cli-app/src/core/executors/command.executor.ts
@@ -0,0 +1,20 @@
+import {IStreamLogger} from "../handlers/stream.type";
+import {ChildProcessWithoutNullStreams} from "child_process";
+import {ICommand} from "./command.types";
+
+export abstract class CommandExecutor<Input> {
+    constructor(private logger: IStreamLogger) {}
+
+    public async execute() {
+        const input = await this.prompt();
+        const command = this.build(input);
+        const stream = this.spawn(command);
+        this.processStream(stream, this.logger)
+    }
+
+    protected abstract prompt(): Promise<Input>;
+    protected abstract build(input: Input): ICommand;
+    protected abstract spawn(command: ICommand): ChildProcessWithoutNullStreams;
+    protected abstract processStream(stream: ChildProcessWithoutNullStreams, logger: IStreamLogger): void;
+
+}
